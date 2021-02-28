f9680df87a3f5f89f5c91ada5e07a197623fac1b
Manuel Perez <Mperez5891@gmail.com>
2021-02-28 14:41:08 -0800

    Add files via upload
    Adding a project 2 folder

---
+++ Project_02/Main.py
@@ -0,0 +1,46 @@
+# Authors: 
+# Abhishek Sunil Gujamagdi
+# Divya Barsode
+# Manuel Perez
+#
+# Project: 
+#
+# Date: 3/9/2021
+#
+
+# User services:
+# ========================================================
+def createUser(username, email, password):
+# Registers a new user account. Returns true if 
+# username is available, email address is valid, 
+# and password meets complexity requirements.
+
+def checkPassword(username, password):
+# Returns true if the password parameter matches 
+# the password stored for the username.
+
+def addFollower(username, usernameToFollow):
+# Start following a new user.
+
+def removeFollower(username, usernameToRemove):
+# Stop following a user.
+
+# Timelines service:
+# ========================================================
+def getUserTimeline(username):
+# Returns recent posts from a user.
+
+def getPublicTimeline():
+#Returns recent posts from all users.
+
+def getHomeTimeline(username):
+# Returns recent posts from all users that this user follows.
+
+def postTweet(username, text):
+# Post a new tweet.
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
